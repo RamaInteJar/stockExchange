bfafd19e0b9364efe03b80da8cc8b0b55617b79c
pentiumKing <ramahawai87@gmail.com>
2023-10-07 09:25:04 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Homepage from './pages/Homepage'
+import About from './pages/About'
+import Stocklisting from './pages/Stocklisting'
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello</h1>
+    <div>
+      <h1>Vipi wewe</h1>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/About' element={<About/>}/>
+        <Route path='/Stocklisting' element={<Stocklisting/>}/>
+        <Route/>
+      </Routes>
+     
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <Link to="/">Homepage</Link>
+            <Link to="About">About</Link>
+            <Link to="Stocklisting">Stocklist</Link>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
