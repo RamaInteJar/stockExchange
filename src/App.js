248ce58f1704fa6595ebec6d507ef35f3d8fbd0d
pentiumKing <ramahawai87@gmail.com>
2023-10-07 16:34:11 -0400

    debbuging fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import Homepage from './pages/Homepage'
-import About from './pages/About'
-import Stocklisting from './pages/Stocklisting'
-import {Route, Routes} from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import About from "./pages/About";
+import Stocklisting from "./pages/Stocklisting";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <h1>Vipi wewe</h1>
-      <Navbar/>
+        <Navbar />
       <Routes>
-        <Route path='/' element={<Homepage/>}/>
-        <Route path='/About' element={<About/>}/>
-        <Route path='/Stocklisting' element={<Stocklisting/>}/>
-        <Route/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Stocklisting" element={<Stocklisting />}/>
       </Routes>
-     
     </div>
   );
 }
